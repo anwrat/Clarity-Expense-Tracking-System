50273a0e32d3304b10aa19ce535586393df5f1d7
anwrat <anweshrwt22@gmail.com>
2026-02-10 18:02:43 +0545

    Endpoint for fetching all transactions

--- backend/src/controllers/transaction.controller.ts
+++ backend/src/controllers/transaction.controller.ts
@@ -18,9 +18,28 @@ export const addTransaction = async(req: Request, res: Response) =>{
                 userId
             },
         });
-        res.status(201).json({success: true, message: "Successfully added transaction", data: transaction});
+        res.status(200).json({success: true, message: "Successfully added transaction", data: transaction});
     }catch(err){
         console.error(err);
         return res.status(500).json({success: false, message: "Internal Server Error while adding transaction"});
+    }
+}
+
+export const getAllTransactions = async(req: Request, res: Response)=>{
+    try{
+        const userId = req.user?.id;
+        if(!userId){
+            return res.status(401).json({success: false, message: "Unauthorized: No user Id found"});
+        }
+        const transactions = await prisma.transaction.findMany({
+            where:{userId:userId},
+            orderBy: {
+                date: "desc",
+            }
+        });
+        return res.status(200).json({success: true, message: "Successfully fetched transactions", data: transactions});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({success: false, message: "Internal Server Error while fetching all transactions"});
     }
 }
--- backend/src/routes/transaction.routes.ts
+++ backend/src/routes/transaction.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addTransaction } from "../controllers/transaction.controller.js";
+import { addTransaction, getAllTransactions } from "../controllers/transaction.controller.js";
 import { authenticateToken } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/zodValidate.middleware.js";
 import { transactionSchema } from "../schemas/transaction.schema.js";
@@ -7,5 +7,6 @@ import { transactionSchema } from "../schemas/transaction.schema.js";
 const router = Router();
 
 router.post('/',authenticateToken,validate(transactionSchema),addTransaction);
+router.get('/',authenticateToken,getAllTransactions);
 
 export default router;
